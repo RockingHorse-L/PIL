4071db109467149cac98529dbe09c687eddd503e
shurui <ShuRuiy@163.com>
2023-03-16 11:32:42 +0800

    第三次更新 ver1.2

--- zuoye05.py
+++ zuoye05.py
@@ -5,22 +5,24 @@ def handleImgMethod02():
     # 切割
     w = size[0] // 2
     h = size[1] // 2
-    count = 0
-    print(w, h)
+    # count = 0
+    # print(w, h)
     imgDic = {}
     for i in range(2):
         for j in range(2):
-            count += 1
+            # count += 1
             box = (w * j, h * i, w * (j + 1), h * (i + 1))
             # 存在字典
             image = img.crop(box)
             imgDic[box] = image
             # image.save(f'./crop_img/{count}.png')
-            # image.show()
+            image.show()
     print(imgDic)
 
     #合并
-    bgImg = Image.new(mode="RGB", size=(w, h))
+    bgImg = Image.new(mode="RGB", size=(size[0], size[1]))
     for box, img in imgDic.items():
         bgImg.paste(img, box=box)
     bgImg.show()
+
+handleImgMethod02()
